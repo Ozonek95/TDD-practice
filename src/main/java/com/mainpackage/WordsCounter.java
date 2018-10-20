17ff68b322a7be67f2667e17c47aed97e4a7664c
Kacper <kacperstaszek1995@gmail.com>
2018-10-20 14:58:46 +0200

    Created Map

--- src/main/java/com/mainpackage/WordsCounter.java
+++ src/main/java/com/mainpackage/WordsCounter.java
@@ -1,21 +1,27 @@
 package com.mainpackage;
 
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class WordsCounter {
-    private String storedString = "";
-    private int counter = 0;
+    Map<String, Integer> valuesMap = new HashMap<String, Integer>();
+
     public int howManyTimesWordWasGiven(String input) {
-        if(input.equals(storedString)){
-            return counter;
+        if(!valuesMap.containsKey(input)){
+            return 0;
         }
-        return 0;
+        else {
+            return valuesMap.get(input);
+        }
     }
 
     public void addWord(String input) {
-        if(counter==0){
-            storedString=input;
+        if (valuesMap.containsKey(input)) {
+            valuesMap.put(input, valuesMap.get(input) + 1);
+        } else {
+            valuesMap.put(input, 1);
         }
-
-        counter++;
     }
 }
